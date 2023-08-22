496b9789e459050a3ef431dfebf095fa30e3d902
Jonathan P. Cruz <JonathanPachecoCruz@hotmail.com>
2023-08-22 16:37:15 -0600

    feat(addStructure): Added the structure of the project

---
+++ src/test/java/com/w34a/crudjava/CrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w34a.crudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
